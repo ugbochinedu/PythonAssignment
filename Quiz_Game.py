4e0b62f0e73c8db062ed6a28c2c427b38b430dd7
Chinedu Ugbo <o.ugbochinedu@yahoo.com>
2023-01-18 22:14:23 +0100

    new file

---
+++ Quiz_Game.py
@@ -0,0 +1,35 @@
+#Prompt a quaetion to the user
+#Collect answer from user as input
+#Check if the answer is correct or wrong
+
+
+print ("Select 1 to 3 for each question")
+selected_answer = input ("Question 1: What is the product of 3 and 5? 1)23 (2)20 (3)15\n")
+if (selected_answer == "1"): 
+	print("failed")
+
+elif (selected_answer == "2"):
+	print("failed")
+
+else:
+	print("correct")
+
+selected_answer = input ("Question 2: What is the sum of 3 and 5? 1)23 (2)2 (3)8\n")
+if (selected_answer) == "1": 
+	print("failed")
+
+elif (selected_answer == "2"):
+	print("failed")
+
+else:
+	print("correct")
+
+selected_answer = input ("Question 3: What is the difference between 5 and 2? 1)2 (2)20 (3)3\n")
+if (selected_answer == "1"): 
+	print("failed")
+
+elif (selected_answer == "2"):
+	print("failed")
+
+else:
+	print("correct")

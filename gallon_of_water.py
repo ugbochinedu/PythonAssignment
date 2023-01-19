4140e992bf0b4fdb47a10869d16067ca5838e88d
Chinedu Ugbo <94117062+ugbochinedu@users.noreply.github.com>
2023-01-19 16:59:25 +0100

    Add files via upload

---
+++ gallon_of_water.py
@@ -0,0 +1,11 @@
+
+inch_rain_str = print("Enter number of inches of rainfall")
+
+user_input = input()
+
+str_rainfall = int(user_input)
+float_user_input = float(user_input)
+
+gallon_of_water_per_acre = float_user_input * 27154
+
+print("The answer is ",gallon_of_water_per_acre, "gallons")
